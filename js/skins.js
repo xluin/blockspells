32713ca535622218750ebc6077cc3f60792a37d2
xluin <138404940+xluin@users.noreply.github.com>
2023-08-02 12:20:06 +0200

    Added new skin

--- js/skins.js
+++ js/skins.js
@@ -4,5 +4,11 @@ let skins = [
         id: "stevie",
         src: "assets/skins/stevie.png",
         vip: false
+    },
+    {
+        type: "skin",
+        id: "tim",
+        src: "assets/skins/tim.png",
+        vip: false
     }
 ]
